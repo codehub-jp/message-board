76f341a7b24875e3c943c2d62260ea43e53268af
EC2 Default User <ec2-user@ip-172-31-90-200.ec2.internal>
2019-11-01 08:43:05 +0000

    CRUD messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'messages#index'
+  
+  resources :messages
+  
+=begin
+
+  get 'messages/:id', to: 'messages#show' #←詳細ページ(show)にアクセスするには、一覧ページ(index)が必要
+  post 'messages', to: 'messages#create' #←保存アクション(create)にデータを送るには、新規作成用のフォームページ(new)が必要
+  put 'messages/:id', to: 'messages#update' #←更新アクション(update)にデータを送るには、更新用のフォームページ（edit）が必要
+  delete 'messages/:id', to: 'messages#destroy' #←ボタンを設置するだけ
+  
+  # index: show の補助ページ
+  get 'messages', to: 'messages#index'
+
+  # new: 新規作成用のフォームページ
+  get 'messages/new', to: 'messages#new'
+
+  # edit: 更新用のフォームページ
+  get 'messages/:id/edit', to: 'messages#edit'
+  
+=end  
+  
 end
